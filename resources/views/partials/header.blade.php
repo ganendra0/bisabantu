44757e368159eb14aa93dabb3b6d18528c106a8d
Ganendra Jawa <ganendrajawara0@gmail.com>
2025-07-17 14:47:20 +0700

    ganti judul

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,7 +3,7 @@
         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon">
             <path d="m8 3 4 8 5-5 5 15H2L8 3z"/>
         </svg>
-        <span class="logo-text">Donasi Sosial</span>
+        <span class="logo-text">Bisabantu</span>
     </a>
     
     {{-- NAVIGASI DESKTOP --}}
